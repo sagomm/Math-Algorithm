da2dc519bbb7bf22d5264facd8d1126525ab42dc
root <root@localhost.localdomain>
2016-03-23 19:40:46 +0800

    蒙提霍尔三门

---
+++ MontyHall.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <stdlib.h> 
+#include <time.h> 
+
+using namespace std;
+
+bool MontyHall(int dselect,int dchange){
+	int dcar = (rand()%2);
+	return false;	
+}
+int main(int argc, char const *argv[])
+{
+	
+	
+	return 0;
+}
