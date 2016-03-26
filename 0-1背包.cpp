e3b4c9580aeb237244cf4d8689e2ea410329c64d
root <root@localhost.localdomain>
2016-03-26 17:32:38 +0800

    3.26

--- 0-1背包.cpp
+++ 0-1背包.cpp
@@ -1,6 +1,9 @@
 #include "iostream"
 #include "vector"
 using namespace std;
+#ifndef MAX_WEIGTH
+#define MAX_WEIGTH 10000
+#endif
 
 class Object
 {
@@ -16,12 +19,12 @@ public:
 		return is;
 	}
 	friend ostream& operator <<(ostream& os,Object& obj){
-		os << "该物品的重量是: "<< obj.weigth <<"  "<< "该物品的价值是:"<< obj.value << endl;
+		os << "该物品的重量是: "<< obj.weigth <<"  "<< "该物品的价值是: "<< obj.value << endl;
 		return os;
 	}
 	int weigth = 0;
 	int value = 0;
-	bool status = 0;
+	bool statu = 0;
 };
 class Knapsack
 {
@@ -29,10 +32,64 @@ public:
 	vector<Object> objs;
 	int total = 0; 
 };
+int planA(Knapsack pack,int limit){
+	int value = 0;
+	int _value = 0;
+	while(_value > limit){
+		value = _value;
+		int best = 0;
+		for(int i = pack.objs.begin();i != pack.objs.end();i++){
+			if((pack.objs[i].value > best) && (pack.objs[i].statu == 0)){
+				best = pack.objs[i].value;
+				pack.objs[i].statu = 1;
+			}
+		}
+		_value += best;	
+	}
+	return value;
+}
+int planB(pack Knapsack,int limit){
+	int value = 0;
+	int _value = 0;
+	while(_value > limit){
+		value = _value;
+		int best = MAX_WEIGTH;
+		for(int i = pack.objs.begin();i != pack.objs.end();i++){
+			if((pack.objs[i].weigth < best) && (pack.objs[i].statu == 0)){
+				best = pack.objs[i].value;
+				pack.objs[i].statu = 1;
+			}
+		}
+		_value += best;	
+	}
+	return value;
+}
+
+int planC(pack Knapsack,int limit){
+	int value = 0;
+	int _value = 0;
+	while(_value > limit){
+		value = _value;
+		int best = MAX_WEIGTH;
+		for(int i = pack.objs.begin();i != pack.objs.end();i++){
+			if(((pack.objs[i].weigth/pack.objs[i].value) > best) && (pack.objs[i].statu == 0)){
+				best = pack.objs[i].value;
+				pack.objs[i].statu = 1;
+			}
+		}
+		_value += best;	
+	}
+	return value;
+}
+int planD(pack Knapsack,int limit){
+	
+}
+
 int main()
 {
 	int total;
 	int limit;
+	Knapsack _knapsack;
 	Knapsack knapsack;
 	cout << " 请输入物品总数:\n";
 	cin >> total;
@@ -40,13 +97,16 @@ int main()
 	for(int i = 0;i<total;i++){
 		Object _tmp;
 		if(cin >> _tmp){
-			knapsack.objs.push_back(_tmp);
+			_knapsack.objs.push_back(_tmp);
 		}
 	}
-	
-	cin >> _tmp;
 	cout << "请输入背包重量限制:\n";
 	cin >> limit;
-	cout << _tmp;
+	
+	cout << planA() << endl;
+	cout << planB() << endl;
+	cout << planC() << endl;
+	cout << placD() << endl;
+
 	return 0;
 }
